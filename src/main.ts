37c414006a0532f018b8f3d3704770937f339307
Alkdess <yosoykam@outlook.com>
2025-08-06 00:03:41 -0500

    Cambios en main y app.config

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-//import { appConfig } from './app/app.config';
+import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-export const appConfig:ApplicationConfig = {
-  providers: [
-    provideHttpClient()
-  ]
-};
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
