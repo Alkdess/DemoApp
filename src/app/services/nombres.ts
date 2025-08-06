9d0327902fe14f4cada630c6bbe4a7856cb6df31
Alkdess <yosoykam@outlook.com>
2025-08-05 23:52:38 -0500

    Nuevo Cambio

--- src/app/services/nombres.ts
+++ src/app/services/nombres.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; 
-import { Observable } from 'rxjs';
+import { catchError } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 
 @Injectable({
@@ -11,6 +12,11 @@ export class ListNombres {
   private apiUrl = 'https://localhost:7279/api/Nombres';
   constructor(private http: HttpClient) { }
   getNombres():Observable<string[]>{
-    return this.http.get<string[]>(this.apiUrl);
+    return this.http.get<string[]>(this.apiUrl).pipe(
+      catchError(error => {
+        console.error('Error obteniendo la lista de nombres:', error);
+        return of([]); // Return an empty array on error
+      })
+    );
   }
 }
